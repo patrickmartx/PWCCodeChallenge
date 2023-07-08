2acf1d5fe978c6b24a33474c5e7d5435dc20d3a7
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-07-08 20:08:54 -0300

    Alteração feita para mudar a forma com que o método é chamado. não sendo mais static. a classe foi declarada no programa principal.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 public class Main {
 
 	public static void main(String[] args) {
+		manipulaString operacaoEmString = new manipulaString();
 		// Desafio 1
 		String firstInput = "Hello, World! OpenAI is amazing.";
-		manipulaString.printaEmOrdemInvertida(firstInput);
-		System.out.println("Certo");
-
+		operacaoEmString.printaEmOrdemInvertida(firstInput);
+		
+		System.out.println();
+		
+		// Desafio 2
+		
 	}
 
 }
--- src/manipulaString.java
+++ src/manipulaString.java
@@ -1,11 +1,15 @@
 public class manipulaString {
+	
+	public void manipulaString() {};
+	
 	// Desafio 1
-	public static void printaEmOrdemInvertida(String frase) {
+	public void printaEmOrdemInvertida(String frase) {
 		String[] palavras = frase.split(" ");
 		for (int posicaoPalavra = palavras.length - 1; posicaoPalavra >= 0; posicaoPalavra--) {
 			System.out.print(palavras[posicaoPalavra] + " ");
 		}
 		System.out.println();
 	}
-
+		
+	// Desafio 2
 }
