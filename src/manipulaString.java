a2364be009a7c93531101ce1c7938445ebf8ba88
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-07-08 20:47:24 -0300

    Foi criado método que recebe uma frase, e printa a mesma sem repetição de caracteres.

--- src/manipulaString.java
+++ src/manipulaString.java
@@ -13,4 +13,16 @@ public class manipulaString implements ImanipulaString {
 	}
 		
 	// Desafio 2
+	@Override
+	public void printaSemRepeticaoDeCaractere(String frase) {
+		StringBuilder fraseSemRepeticao = new StringBuilder();
+		for (int posicaoLetra = 0; posicaoLetra < frase.length(); posicaoLetra++) {
+			char caractereAtual = frase.charAt(posicaoLetra);
+			// Se o caractere não se encontra em nenhuma posição de fraseSemRepeticao.
+			if (fraseSemRepeticao.indexOf(String.valueOf(caractereAtual)) == -1) {
+				fraseSemRepeticao.append(caractereAtual);
+			}
+		}
+		System.out.println(fraseSemRepeticao);
+	}
 }
