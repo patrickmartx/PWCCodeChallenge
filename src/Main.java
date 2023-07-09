9cb162095dd72945f4e29006087f229a346523f5
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-07-08 21:19:51 -0300

    Foi criado uma chamada para o método que printa a frase capitalizando as palavras pós ponto.

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,17 @@ public class Main {
 		// Desafio 2
 		String secondInput = "Hello, World!";
 		operacaoEmString.printaSemRepeticaoDeCaractere(secondInput);
+		
+		System.out.println();
+		
+		// Desafio 3
+		
+		
+		System.out.println();
+		
+		// Desafio 4
+		String fourthInput = "hello. how are you? i'm fine, thank you.";
+		operacaoEmString.printaPrimeiraLetraCapitalizada(fourthInput);
 	}
 
 }
