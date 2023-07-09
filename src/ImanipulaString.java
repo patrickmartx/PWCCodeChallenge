14e69ce445158de845676644a28e633d9910f075
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-07-08 21:19:07 -0300

    Declarado método de printar uma frase capitalizando toda palavra pós ponto.

--- src/ImanipulaString.java
+++ src/ImanipulaString.java
@@ -3,4 +3,8 @@ public interface ImanipulaString {
 	public void printaEmOrdemInvertida(String frase);
 	// Desafio 2
 	public void printaSemRepeticaoDeCaractere(String frase);
+	// Desafio 3
+	
+	// Desafio 4
+	public void printaPrimeiraLetraCapitalizada(String frase);
 }
